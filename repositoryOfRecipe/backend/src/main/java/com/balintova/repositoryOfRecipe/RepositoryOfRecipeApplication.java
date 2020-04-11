cc587483364c892d6ebf2d7ff4e396cd7bc77af8
Iveta <IVbalintova@gmail.com>
2020-04-11 12:13:58 +0200

    priecinok na aplikaciu

---
+++ repositoryOfRecipe/backend/src/main/java/com/balintova/repositoryOfRecipe/RepositoryOfRecipeApplication.java
@@ -0,0 +1,13 @@
+package com.balintova.repositoryOfRecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryOfRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositoryOfRecipeApplication.class, args);
+	}
+
+}
---
+++ repositoryOfRecipe/backend/src/test/java/com/balintova/repositoryOfRecipe/RepositoryOfRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balintova.repositoryOfRecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryOfRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
